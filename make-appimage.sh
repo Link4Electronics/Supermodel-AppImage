83e0e3e5d9f6056ef1e49f895bdf7f1576148a92
Link4Electronics <link4electronics@gmail.com>
2026-02-07 08:28:17 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -3,16 +3,15 @@
 set -eu
 
 ARCH=$(uname -m)
-VERSION=$(pacman -Q PACKAGENAME | awk '{print $2; exit}') # example command to get version of application here
+VERSION=$(pacman -Q supermodel-git | awk '{print $2; exit}') # example command to get version of application here
 export ARCH VERSION
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export ICON=PATH_OR_URL_TO_ICON
-export DESKTOP=PATH_OR_URL_TO_DESKTOP_ENTRY
+export ICON=https://raw.githubusercontent.com/trzy/Supermodel/refs/heads/master/Docs/Images/Real3D_Logo.png
 
 # Deploy dependencies
-quick-sharun /PATH/TO/BINARY_AND_LIBRARIES_HERE
+quick-sharun /usr/bin/supermodel
 
 # Additional changes can be done in between here
 
